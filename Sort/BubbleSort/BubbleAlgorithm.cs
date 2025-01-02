095a309a8ab3aded9b48240d013e2a0680b247b7
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-02 19:48:44 +0700

    bubble sort

---
+++ Sort/BubbleSort/BubbleAlgorithm.cs
@@ -0,0 +1,79 @@
+﻿namespace BubbleSort
+{
+    internal class BubbleAlgorithm<T>
+    {
+        private T[] array;
+        private Func<T, T, bool> compare;
+        public BubbleAlgorithm(T[] array, Func<T, T, bool> compare)
+        {
+            this.array = array;
+            this.compare = compare;
+        }
+
+        public BubbleAlgorithm(Func<T, T, bool> compare)
+        {
+            this.array = new T[0];
+            this.compare = compare;
+        }
+
+        private void Swap(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public void ReadArrayFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            array = new T[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    array[i] = (T)(object)lines[i];
+                }
+                else
+                {
+                    array[i] = (T)Convert.ChangeType(lines[i], typeof(T));
+                }
+            }
+        }
+
+        public void PrintArray()
+        {
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public void BubbleSort()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (compare(array[j], array[j + 1]))
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+            PrintSolution();
+        }
+
+        public void PrintSolution()
+        {
+            Console.WriteLine("Sorted array:");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sort/BubbleSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using BubbleSort;
+
+BubbleAlgorithm<int> bubble = new BubbleAlgorithm<int>((x, y) => x > y); //bieu thuc lamda, (x<y) la giam dan, (x>y) la tang dan doi voi int
+bubble.ReadArrayFromFile("../../../testcase1.txt");
+bubble.PrintArray();
+bubble.BubbleSort();
+
+bubble.ReadArrayFromFile("../../../testcase2.txt");
+bubble.PrintArray();
+bubble.BubbleSort();
+
+BubbleAlgorithm<string> bubbleString = new BubbleAlgorithm<string>((x, y) => string.Compare(x, y) > 0);
+bubbleString.ReadArrayFromFile("../../../testcase3.txt");
+bubbleString.PrintArray();
+bubbleString.BubbleSort();
