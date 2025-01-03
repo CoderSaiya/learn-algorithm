85c10ca9f7687c1c1101447e8d306ac5dc902bfc
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-03 18:58:16 +0700

    Insertion Sort

---
+++ Sort/InsertionSort/InsertionAlgorithm.cs
@@ -0,0 +1,76 @@
+﻿namespace InsertionSort
+{
+    internal class InsertionAlgorithm<T>
+    {
+        private T[] array;
+        private Func<T, T, bool> compare;
+
+        public InsertionAlgorithm(T[] array, Func<T, T, bool> compare)
+        {
+            this.array = array;
+            this.compare = compare;
+        }
+
+        public InsertionAlgorithm(Func<T, T, bool> compare)
+        {
+            this.array = new T[0];
+            this.compare = compare;
+        }
+
+        public void ReadArrayFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            array = new T[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    array[i] = (T)(object)lines[i];
+                }
+                else
+                {
+                    array[i] = (T)Convert.ChangeType(lines[i], typeof(T));
+                }
+            }
+        }
+
+        public void PrintArray()
+        {
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public void InsertionSort()
+        {
+            for(int i = 1; i < this.array.Length; i++)
+            {
+                T key = this.array[i];
+                int j = i - 1;
+
+                while (j >= 0 && compare(array[j], key))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+            PrintSolution();
+        }
+
+        public void PrintSolution()
+        {
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sort/InsertionSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using InsertionSort;
+
+InsertionAlgorithm<int> insertion = new InsertionAlgorithm<int>((x, y) => x > y);
+insertion.ReadArrayFromFile("../../../testcase1.txt");
+insertion.PrintArray();
+insertion.InsertionSort();
+
+insertion.ReadArrayFromFile("../../../testcase2.txt");
+insertion.PrintArray();
+insertion.InsertionSort();
+
+InsertionAlgorithm<string> insertionString = new InsertionAlgorithm<string>((x , y) => string.Compare(x, y) > 0);
+insertionString.ReadArrayFromFile("../../../testcase3.txt");
+insertionString.PrintArray();
+insertionString.InsertionSort();
