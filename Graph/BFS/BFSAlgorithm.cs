61853e9865a908e8038315935853f54a23892f5d
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-07 12:24:30 +0700

    Breadth-First Search

---
+++ Graph/BFS/BFSAlgorithm.cs
@@ -0,0 +1,51 @@
+﻿using DFS;
+
+namespace BFS
+{
+    internal class BFSAlgorithm
+    {
+        private readonly Graph graph;
+        private readonly HashSet<int> visited;
+        private readonly Queue<int> queue;
+
+        public BFSAlgorithm(Graph graph)
+        {
+            this.graph = graph;
+            this.visited = new HashSet<int>();
+            this.queue = new Queue<int>();
+        }
+        
+        public void Traverse(int startVertex)
+        {
+            if (!graph.GetAdjacencyList().ContainsKey(startVertex))
+            {
+                Console.WriteLine($"Đỉnh {startVertex} không tồn tại trong đồ thị.");
+                return;
+            }
+
+            var visited = new HashSet<int>();
+            var queue = new Queue<int>();
+
+            visited.Add(startVertex);
+            queue.Enqueue(startVertex);
+
+            Console.WriteLine($"Duyệt BFS từ đỉnh {startVertex}:");
+
+            while (queue.Count > 0)
+            {
+                int currentVertex = queue.Dequeue();
+
+                Console.WriteLine($"Đang xử lý đỉnh {currentVertex}");
+
+                foreach (var (neighbor, _) in graph.GetAdjacencyList()[currentVertex])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        visited.Add(neighbor);
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Graph/BFS/Program.cs
@@ -0,0 +1,14 @@
+﻿var graph = new DFS.Graph();
+
+graph.AddEdge(1, 2, 5);
+graph.AddEdge(1, 3, 5);
+graph.AddEdge(2, 4, 2);
+graph.AddEdge(3, 5, 1);
+graph.AddEdge(4, 6, 3);
+
+Console.WriteLine("Đồ thị:");
+graph.PrintGraph();
+
+var bfs = new BFS.BFSAlgorithm(graph);
+
+bfs.Traverse(1);
